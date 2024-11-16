3d02e7b63e02b5c2a6e893df0314da1e428afa3e
SkyChow8589 <skychow8589@gmail.com>
2024-11-16 16:29:45 -0600

    Preparing for Deployment

--- backend/util/location.js
+++ backend/util/location.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const HttpError = require("../models/http-error");
 
-const API_KEY = "AIzaSyBBkkVUXjEhNzU-LchDH-9fJXd1CH26IX0";
+const API_KEY = process.env.GOOGLE_API_KEY;
 
 async function getCoordsForAddress(address) {
   const response = await axios.get(
